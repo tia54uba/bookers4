c44ef22492343039199b8ec828400a96e74557af
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-08-02 14:09:29 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/show'
-  get 'index/new'
-  get 'index/create'
-  get 'index/show'
+  
   devise_for :users
   root to:'homes#top'
+  
+    resources :posts, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

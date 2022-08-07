813d1a3047d5fcca19401dea069894d9e931724c
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-08-07 13:41:24 +0000

    update post new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,22 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
   end
 
   def show
   end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:match_date_time,:category,:home_team,:away_team,:body)
+  end    
 end
